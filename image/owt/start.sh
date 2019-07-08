339503b16ac906173fd8a1833f051090c511d51a
Qiujiao <qiujiao.wu@intel.com>
2019-07-08 17:45:08 +0800

    Update start script (#37)

--- image/owt/start.sh
+++ image/owt/start.sh
@@ -3,17 +3,18 @@
 sed -i "s#127.0.0.1#0.0.0.0#" /etc/mongodb.conf &
 service mongodb start &
 service rabbitmq-server start &
-while ! mongo --quiet --eval \"db.adminCommand('listDatabases')\"
+while ! mongo --quiet --eval "db.adminCommand('listDatabases')"
 do
     sleep 1
 done
 
 if [ -n "$1" ]; then
 echo $1
-repacestr="network_interfaces = [{name = \"eth0\", replaced_ip_address = \"${$1}\"}]"
+externalip=$1
+repacestr="network_interfaces = [{name = \"eth0\", replaced_ip_address = \"${externalip}\"}]"
 
-sed -i "s/network_interfaces = \[\]/${repacestr}/" webrtc_agent/agent.toml
-sed -i "/^ip_address = /c \ip_address = \"${$1}\"" portal/portal.toml
+sed -i "s/network_interfaces = \[\]/${repacestr}/" /home/owt/webrtc_agent/agent.toml
+sed -i "/^ip_address = /c \ip_address = \"${externalip}\"" /home/owt/portal/portal.toml
 fi
 
 cd /home/owt

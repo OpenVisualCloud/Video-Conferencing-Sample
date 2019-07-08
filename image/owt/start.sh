832b24988701a9b1de24a66dac1c9d025df75244
qwu16 <qwu16@intel.com>
2019-07-08 16:19:49 +0800

    Update script

--- image/owt/start.sh
+++ image/owt/start.sh
@@ -8,11 +8,13 @@ do
     sleep 1
 done
 
+if [ -n "$1" ]; then
+echo $1
 repacestr="network_interfaces = [{name = \"eth0\", replaced_ip_address = \"${$1}\"}]"
 
 sed -i "s/network_interfaces = \[\]/${repacestr}/" webrtc_agent/agent.toml
 sed -i "/^ip_address = /c \ip_address = \"${$1}\"" portal/portal.toml
-
+fi
 
 cd /home/owt
 ./management_api/init.sh && ./bin/start-all.sh 
--- web-meeting/self-sign.sh
+++ web-meeting/self-sign.sh
@@ -2,6 +2,12 @@
 
 DIR=$(dirname $(readlink -f "$0"))
 
+DIRECTORY=${DIR}/app-server/cert
+
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir ${DIRECTORY}
+fi
+
 openssl req -x509 -nodes -days 180 -newkey rsa:2048 -keyout $DIR/app-server/cert/key.pem -out $DIR/app-server/cert/cert.pem << EOL
 CN
 SH

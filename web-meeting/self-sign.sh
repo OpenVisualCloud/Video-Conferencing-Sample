5864edf2b01212b23d48fb5fe4381a4ed7848c41
Qiujiao <qiujiao.wu@intel.com>
2019-07-09 10:33:22 +0800

    Add directory for self signed certificate (#38)

--- web-meeting/self-sign.sh
+++ web-meeting/self-sign.sh
@@ -2,18 +2,19 @@
 
 DIR=$(dirname $(readlink -f "$0"))
 
-DIRECTORY=${DIR}/app-server/cert
+hostname=$1
+directory=$2
 
-if [ ! -d "$DIRECTORY" ]; then
-    mkdir ${DIRECTORY}
+if [ ! -d "$directory" ]; then
+    mkdir ${directory}
 fi
 
-openssl req -x509 -nodes -days 180 -newkey rsa:2048 -keyout $DIR/app-server/cert/key.pem -out $DIR/app-server/cert/cert.pem << EOL
+openssl req -x509 -nodes -days 180 -newkey rsa:2048 -keyout ${directory}/key.pem -out ${directory}/cert.pem << EOL
 CN
 SH
 Shanghai
 Shanghai
 owt-demo
-$1
+${hostname}
 nobody@owt.org
 EOL
